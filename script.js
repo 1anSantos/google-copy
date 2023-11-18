2184ae1bb07f0bfabacfcea9fe08b98eb8a47e5f
Ian Santos <iann.silva2006@gmail.com>
2023-11-18 16:19:02 -0300

    link target="_self"

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-// VAR
-const body = document.body;
-const titlePage = document.querySelector("title");
+//* Variables 
+    // TAGS
+    const body = document.body;
+    const titlePage = document.querySelector("title");
 
-// ID
-const logo = document.querySelector("#logo img");
-const search = document.querySelector("#search");
-const settings = document.querySelector("#settings");
-const searchBTN = document.querySelector("#search-button");
-const settingsBoard = document.querySelector("#settings-board");
-const imFeelingLucktBTN = document.querySelector("#im-feeling-lucky-button");
+    // ID
+    const logo = document.querySelector("#logo img");
+    const search = document.querySelector("#search");
+    const settings = document.querySelector("#settings");
+    const searchBTN = document.querySelector("#search-button");
+    const settingsBoard = document.querySelector("#settings-board");
+    const imFeelingLucktBTN = document.querySelector("#im-feeling-lucky-button");
 
-// Class
-const changeLogo = document.querySelector(".change-logo");
-const changeTheme = document.querySelector(".change-theme");
-const magnifyingGlass = document.querySelector("div.magnifying-glass");
+    // Class
+    const changeLogo = document.querySelector(".change-logo");
+    const changeTheme = document.querySelector(".change-theme");
+    const magnifyingGlass = document.querySelector("div.magnifying-glass");
 
-// Functions
+//* Event Listeners 
 settings.addEventListener("click", () => {
     if (settingsBoard.style.display === "none") {
         settingsBoard.style.display = "block";
@@ -51,21 +52,21 @@ magnifyingGlass.addEventListener("click", () => {
     search.focus();
 });
 
-searchBTN.addEventListener("click", () => {
+search.addEventListener("keyup", (event) => {
+    if (event.key !== "Enter") return;
     searchOnGoogle('https://www.google.com/search?q=');
 });
 
-search.addEventListener("keyup", (event) => {
-    if (event.key === "Enter") {
-        searchOnGoogle('https://www.google.com/search?q=');
-    }
+searchBTN.addEventListener("click", () => {
+    searchOnGoogle('https://www.google.com/search?q=');
 });
 
 imFeelingLucktBTN.addEventListener("click", () => {
     searchOnGoogle('https://www.google.com/search?btnI=1&q=');
 });
 
+//* Functions 
 function searchOnGoogle(googleUrl) {
     googleUrl += encodeURIComponent(search.value);
-    window.open(googleUrl, "_blank");
+    window.open(googleUrl, "_self");
 }
