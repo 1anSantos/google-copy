9828fb4fa4b7ff0700c3d9fecf6edec74dcf95b6
Ian Santos <iann.silva2006@gmail.com>
2023-11-20 14:48:37 -0300

    settigns update

--- script.js
+++ script.js
@@ -2,26 +2,49 @@
 const body = document.querySelector('body');
 const logo = document.querySelector('#logo');
 const settingsBtn = document.querySelector("#settings-btn");
+const changeLogoBtn = document.querySelector("#change-logo-btn");
+const changeThemeBtn = document.querySelector("#change-theme-btn");
 const searchInput = document.querySelector("#search-input");
 const normalSearchBtn = document.querySelector('#normal-search-btn');
 const luckySearchBtn = document.querySelector('#lucky-search-btn');
 const magnifyingGlassBtn = document.querySelector("#search-magnifying-glass");
 
 //* Event Listeners 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     const savedDarkMode = localStorage.getItem('dark-theme');
 
-    if (savedDarkMode === 'true') {
-        logo.src = "/img/google-logo-white.png";
-        document.body.classList.add('dark-theme');
-    }
+    if (savedDarkMode !== "true") return;
+    document.body.classList.add('dark-theme');
 });
 
-// settingsBtn.addEventListener("click", toggleTheme);
-// settingsBtn.addEventListener("keydown", (event) => {
-//     if (event.key !== "Enter") return;
-//     toggleTheme();
-// });
+document.addEventListener("click", (event) => {
+    const isDropdownBtn = event.target.matches("[data-dropdown-btn]");
+    if (!isDropdownBtn && event.target.closest("[data-dropdown]") != null) return;
+
+    let currentDropdown;
+
+    if (isDropdownBtn) {
+        currentDropdown = event.target.closest("[data-dropdown]");
+        currentDropdown.classList.toggle("active");
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return;
+        dropdown.classList.remove("active");
+    });
+});
+
+changeThemeBtn.addEventListener("click", toggleTheme);
+changeThemeBtn.addEventListener("keydown", (event) => {
+    if (event.key !== "Enter") return;
+    toggleTheme();
+});
+
+changeLogoBtn.addEventListener("click", changeLogo);
+changeLogoBtn.addEventListener("keydown", (event) => {
+    if (event.key !== "Enter") return;
+    changeLogo();
+});
 
 magnifyingGlassBtn.addEventListener("click", () => {
     searchInput.focus();
@@ -42,6 +65,20 @@ luckySearchBtn.addEventListener("click", () => {
 
 
 //* Functions 
+function searchOnGoogle(googleUrl) {
+    googleUrl += encodeURIComponent(searchInput.value);
+    window.open(googleUrl, "_self");
+}
+
+function changeLogo() {
+    if (logo.style.content !== "var(--perry-logo)") {
+        logo.style.content = "var(--perry-logo)";
+    }
+    else {
+        logo.style.content = "var(--google-logo)";
+    }
+}
+
 function toggleTheme() {
     body.classList.toggle("dark-theme");
     const isDarkMode = body.classList.contains("dark-theme")
@@ -53,9 +90,4 @@ function toggleTheme() {
         logo.src = "./img/google-logo-colorful.png";
     }
     localStorage.setItem('dark-theme', isDarkMode);
-}
-
-function searchOnGoogle(googleUrl) {
-    googleUrl += encodeURIComponent(searchInput.value);
-    window.open(googleUrl, "_self");
 }
